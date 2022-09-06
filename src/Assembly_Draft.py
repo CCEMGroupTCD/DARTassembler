54ac32c063630205fb818571183bf6362f44f9c5
Abel978 <felix.kleuker@googlemail.com>
2022-09-06 14:23:36 +0100

    22-09-06 5-1 safe before Cian changes

--- src/Assembly_Draft.py
+++ src/Assembly_Draft.py
@@ -100,62 +100,62 @@ def random_assembly(num, ligand_dict: dict, comps: list, safe_path: str, metals,
 
     # for all ligands we want to create
     for i in range(num):
-       # try:
-        # choose random metal center
-        (metal, charge) = random.choice(metals)
+        try:
+            # choose random metal center
+            (metal, charge) = random.choice(metals)
 
-        # build the center atom with 6 connections
-        metal_bb = stk.BuildingBlock(smiles='[Hg+2]',
-                                     functional_groups=(stk.SingleAtom(stk.Hg(0, charge=2)) for i in range(6)),
-                                     position_matrix=np.ndarray([0, 0, 0])
-                                     )
+            # build the center atom with 6 connections
+            metal_bb = stk.BuildingBlock(smiles='[Hg+2]',
+                                         functional_groups=(stk.SingleAtom(stk.Hg(0, charge=2)) for i in range(6)),
+                                         position_matrix=np.ndarray([0, 0, 0])
+                                         )
 
-        # build the metal block with the new metal atom
-        smiles_str = f"[{metal}{charge}]"
-        stk_metal_func = globals()[f"{metal}"]
-        # stk_metal_func = getattr(__import__("stk"), metal)
-        functional_groups = (stk.SingleAtom(stk_metal_func(0, charge=charge)) for i in range(6))
-        final_metal_bb = stk.BuildingBlock(smiles=smiles_str,
-                                           functional_groups=functional_groups,
-                                           position_matrix=np.ndarray([0, 0, 0])
-                                           )
-        #
-        #
-        # chose a random composition of the possible comps
-        comp = random.choice(comps)
+            # build the metal block with the new metal atom
+            smiles_str = f"[{metal}{charge}]"
+            stk_metal_func = globals()[f"{metal}"]
+            # stk_metal_func = getattr(__import__("stk"), metal)
+            functional_groups = (stk.SingleAtom(stk_metal_func(0, charge=charge)) for i in range(6))
+            final_metal_bb = stk.BuildingBlock(smiles=smiles_str,
+                                               functional_groups=functional_groups,
+                                               position_matrix=np.ndarray([0, 0, 0])
+                                               )
+            #
+            #
+            # chose a random composition of the possible comps
+            comp = random.choice(comps)
 
-        # and choose a random set of ligands
-        ligands = {i: random.choice(ligand_dict[index]) for i, index in enumerate(comp)}
+            # and choose a random set of ligands
+            ligands = {i: random.choice(ligand_dict[index]) for i, index in enumerate(comp)}
 
-        # dict: ligand: ligand_bb
-        # nr : (ligand, ligand_bb)
-        ligand_bb_dict = {}
+            # dict: ligand: ligand_bb
+            # nr : (ligand, ligand_bb)
+            ligand_bb_dict = {}
 
-        for k, lig in enumerate(ligands.values()):
-            lig_assembly_dict = lig.get_assembly_dict()
+            for k, lig in enumerate(ligands.values()):
+                lig_assembly_dict = lig.get_assembly_dict()
 
-            xyz_str = lig_assembly_dict["str"]
-            with open("../tmp/lig_xyz.xyz", "w+") as f:
-                f.write(xyz_str)
+                xyz_str = lig_assembly_dict["str"]
+                with open("../tmp/lig_xyz.xyz", "w+") as f:
+                    f.write(xyz_str)
 
-            os.system('obabel .xyz ../tmp/lig_xyz.xyz .mol -O  ../tmp/lig_mol.mol')
+                os.system('obabel .xyz ../tmp/lig_xyz.xyz .mol -O  ../tmp/lig_mol.mol')
 
-            ligand_bb_dict[k] = (lig, build_ligand(type_list=lig_assembly_dict["type"],
-                                                   index_list=lig_assembly_dict["index"],
-                                                   path_="../tmp/lig_mol.mol"))
-            os.remove("../tmp/lig_mol.mol")
+                ligand_bb_dict[k] = (lig, build_ligand(type_list=lig_assembly_dict["type"],
+                                                       index_list=lig_assembly_dict["index"],
+                                                       path_="../tmp/lig_mol.mol"))
+                os.remove("../tmp/lig_mol.mol")
 
-        complex_ = assembly(metal_bb, final_metal_bb, ligand_bb_dict, comp, _optimize=_optimize)
+            complex_ = assembly(metal_bb, final_metal_bb, ligand_bb_dict, comp, _optimize=_optimize)
 
-        post_process_complex(complex_, name={f"{ligands[0].csd_code}"},
-                             visualize_=visualize_, print_to_xyz=True, path=safe_path)
+            post_process_complex(complex_, name={f"{ligands[0].csd_code}"},
+                                 visualize_=visualize_, print_to_xyz=True, path=safe_path)
 
-        generated_complexes.append(complex_)
+            generated_complexes.append(complex_)
 
-        #except Exception as ex:
-            #print(f"An Error has occured: {ex}")
-            #logging.info(f"An Error has occured: {ex}")
-            #pass
+        except Exception as ex:
+            print(f"An Error has occured: {ex}")
+            logging.info(f"An Error has occured: {ex}")
+            pass
 
     return generated_complexes
 
@@ -164,7 +164,7 @@ if __name__ == '__main__':
     # Input setting for generating ligands
     #
     # number of ligands to generate
-    number_of_ligands = 1
+    number_of_ligands = 100
 
     #
     # the ligand dict
@@ -173,9 +173,9 @@ if __name__ == '__main__':
 
     #
     # all denticity combinations
-    possible_compositions = [#(4, 1, 1)  # ,
+    possible_compositions = [(4, 1, 1)  # ,
                              # (3, 2, 1)
-                             (5, 1)
+                             #(5, 1)
                              # (3, 3)
                              # ...
                              ]
@@ -193,7 +193,7 @@ if __name__ == '__main__':
 
     #
     # visualize the constructed molecules during the process
-    visualize_ = True
+    visualize_ = False
 
     #
     # decide wether optimization is necessary
@@ -207,3 +207,4 @@ if __name__ == '__main__':
                                 visualize_=visualize_,
                                 _optimize=optimize_
                                 )
+
