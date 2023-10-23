739a6ea3a1c9a12a61181b4a85598a59a4344c9e
Timo Sommer <tipub@t-online.de>
2023-10-23 12:35:03 +0100

    Updated with full 620 gaussian relaxed complexes

--- examples/Pd_Ni_Cross_Coupling/analyze_complexes/make_plots.py
+++ examples/Pd_Ni_Cross_Coupling/analyze_complexes/make_plots.py
@@ -11,44 +11,55 @@ outdir = 'plots'
 
 
 #======================  Plotting ======================#
+if __name__ == '__main__':
+    # Define names
+    pn_bite_angle = 'P_N_bite_angle'
+    homo = 'homo'
+    lumo = 'lumo'
+    hlgap = 'hlgap'
+    metal_charge = 'metal_charge'
+    metal = 'metal'
 
-df = pd.read_csv(input_data)
-# Remove not converged complexes with NaN values
-df = df.dropna(axis='rows')
+    df = pd.read_csv(input_data)
+    # Remove not converged complexes with NaN values
+    df = df.dropna(axis='rows')
 
-# Histograms
-for name in ['P-N bite angle', 'HOMO', 'LUMO', 'HL_gap', 'Metal charge']:
+    outdir = Path(outdir)
+    outdir.mkdir(parents=True, exist_ok=True)
+
+    # Histograms
+    for name in [pn_bite_angle, homo, lumo, hlgap, metal_charge]:
+        plt.figure()
+        sns.histplot(data=df, x=name, hue=metal, bins=25)
+        plt.xlabel(name)
+        plt.savefig(Path(outdir, f'hist_{name}.svg'))
+        plt.close()
+
+    # 2D scatter plot of Metal-P and Metal-N bond distance with Mulliken charge as hue
+    donor1 = 'P'
+    donor2 = 'N'
     plt.figure()
-    sns.histplot(data=df, x=name, hue='metal')
-    plt.xlabel(name)
-    plt.savefig(Path(outdir, f'hist_{name}.svg'))
+    palette = sns.color_palette("coolwarm", as_cmap=True)
+    markers = ['o', '^']
+    ax = sns.scatterplot(data=df, x=f'dist_{donor1}', y=f'dist_{donor2}', hue=metal_charge, style=metal, palette=palette, alpha=1, markers=markers)
+
+    norm = plt.Normalize(df[metal_charge].min(), df[metal_charge].max())
+    sm = plt.cm.ScalarMappable(cmap=palette, norm=norm)
+    sm.set_array([])
+
+    # Add a colorbar
+    ax.figure.colorbar(sm)
+
+    h, l = ax.get_legend_handles_labels()
+    plt.legend(h[6:], l[6:])
+
+    label = 'Distance'
+    plt.xlabel(f'{label} to {donor1} in Angstroms')
+    plt.ylabel(f'{label} to {donor2} in Angstroms')
+    plt.savefig(Path(outdir, f'scatter_metal_charge_dist_{donor1}_{donor2}.svg'))
     plt.close()
 
-# 2D scatter plot of Metal-P and Metal-N bond distance with Mulliken charge as hue
-donor1 = 'P'
-donor2 = 'N'
-plt.figure()
-palette = sns.color_palette("coolwarm", as_cmap=True)
-markers = ['o', '^']
-ax = sns.scatterplot(data=df, x=f'dist_{donor1}', y=f'dist_{donor2}', hue='Metal charge', style='metal', palette=palette, alpha=1, markers=markers)
 
-norm = plt.Normalize(df['Metal charge'].min(), df['Metal charge'].max())
-sm = plt.cm.ScalarMappable(cmap=palette, norm=norm)
-sm.set_array([])
-
-# Add a colorbar
-ax.figure.colorbar(sm)
-
-h, l = ax.get_legend_handles_labels()
-plt.legend(h[6:], l[6:])
-
-label = 'Distance'
-plt.xlabel(f'{label} to {donor1} in Angstroms')
-plt.ylabel(f'{label} to {donor2} in Angstroms')
-plt.savefig(Path(outdir, f'scatter_metal_charge_dist_{donor1}_{donor2}.svg'))
-plt.close()
-
-
-print('Done!')
+    print('Done!')
 
 
