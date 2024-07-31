0709b51df24b266db469d7e74dacb9fb9378bd9a
Timo Sommer <tipub@t-online.de>
2024-07-31 14:33:56 +0100

    Tried out some conda envs.

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
-    name="DART-Assembly",
+    name="DARTassembler",
     version="0.1",
     description="A software for chemists to build novel metal complexes from random ligands on the computer.",
     url='https://github.com/CCEMGroupTCD/DART',
-    python_requires=">=3.9.5",
+    python_requires=">=3.9",
     author="Timo Sommer, Cian Clarke, Felix Kleuker",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[],    # requirements are handled by conda
     entry_points={
         'console_scripts': [
             'dart=dart_cli:main',
