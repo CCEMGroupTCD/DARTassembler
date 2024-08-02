3a98442400de9698e3989607861c7993e82f36fa
Timo Sommer <tipub@t-online.de>
2024-08-02 15:10:15 +0100

    Moved dart_cli into DARTassembler directory to be installed with pip.

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="DARTassembler",
     version="0.1",
-    description="A software for chemists to build novel metal complexes from random ligands on the computer.",
+    description="Simple to use package for building 3D structures of novel transition metal complexes from a large database of ligands extracted from the Cambridge Structural Database.",
     url='https://github.com/CCEMGroupTCD/DART',
     python_requires=">=3.9",
     author="Timo Sommer, Cian Clarke, Felix Kleuker",
     packages=find_packages(),
-    install_requires=[],    # requirements are handled by conda
+    install_requires=[],    # Requirements are handled by conda. See file conda_DART.yml.
     entry_points={
         'console_scripts': [
-            'dart=dart_cli:main',
+            'DARTassembler=DARTassembler.dart_cli:main',
         ],
     },
 )
