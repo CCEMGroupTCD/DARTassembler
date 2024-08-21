edc5322eb9cb60f890d7943c78d1dbfc347a8aa8
Timo Sommer <tipub@t-online.de>
2024-08-21 15:11:44 +0100

    Fixed that configs module would not create new directory if needed.

--- DARTassembler/configs.py
+++ DARTassembler/configs.py
@@ -14,6 +14,7 @@ def get_default_config_files_saved(outdir: str) -> None:
     print(f"Getting default configuration files...")
 
     outpath = Path(outdir)
+    outpath.mkdir(parents=True, exist_ok=True)
 
     # Copy assembler.yml
     filename = default_assembler_yml_path.name
