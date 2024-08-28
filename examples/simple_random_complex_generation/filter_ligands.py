aca0a30cefc9960eaad843cb3a30cec42d35fa94
Timo Sommer <tipub@t-online.de>
2024-08-28 16:55:47 +0100

    Deleted old files for quickstart guide.

--- examples/simple_random_complex_generation/filter_ligands.py
+++
@@ -1,5 +0,0 @@
-import DARTassembler
-
-assembly = DARTassembler.filter_ligands('ligandfilters_input.yml')
-
-
--- examples/simple_random_complex_generation/generate_complexes.py
+++
@@ -1,5 +0,0 @@
-import DARTassembler
-
-assembly = DARTassembler.assemble_complexes('simple_assembly_input.yml')
-
-
