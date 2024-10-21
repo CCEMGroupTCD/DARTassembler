2446037ced4e8f3627da23f69477a5d816043430
Timo Sommer <tipub@t-online.de>
2024-10-21 17:22:06 +0100

    Try to fix publish workflow by updating url in setup.py from DART to DARTassembler.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="Simple to use package for building 3D structures of novel transition metal complexes from a large database of ligands extracted from the Cambridge Structural Database.",
     long_description=open('README.md').read(),
     long_description_content_type='text/plain',
-    url='https://github.com/CCEMGroupTCD/DART',
+    url='https://github.com/CCEMGroupTCD/DARTassembler',
     python_requires=">=3.9",
     author="Timo Sommer, Cian Clarke, Felix Kleuker",
     packages=find_packages(),
